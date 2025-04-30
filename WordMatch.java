b7dede2842f397811de2fd67fa7a0fc6164bbdb6
Asadulnyc <asadulk@nycstudents.net>
2025-04-30 15:48:47 +0000

    ajsdfhajkshf

---
+++ WordMatch.java
@@ -0,0 +1,17 @@
+public class WordMAtch {
+    private String secret;
+    
+    public WordMatch(String word) {
+        secret = word;
+    }
+
+    public int scoreGuess(String guess) {
+        for (int i = 0; i<=secret.length()-guess.length(); i++) {
+
+        }
+    }
+
+    public String findBetterGuess(String guess1, String guess2) {
+
+    }
+}
